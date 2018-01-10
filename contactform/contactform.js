2a1c186f01197fe356784bdee483072063e5fda1
David <david17sego@gmail.com>
2018-01-10 19:35:17 +0100

    First public version

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -79,21 +79,17 @@ jQuery(document).ready(function($) {
         else var str = $(this).serialize();		
             $.ajax({
                 type: "POST",
-                url: "contactform/contactform.php",
+                url: "contactform/contact.php",
                 data: str,
-                success: function(msg){
-                   // alert(msg);
-                    if(msg == 'OK') {
-                        $("#sendmessage").addClass("show");			
-                        $("#errormessage").removeClass("show");	
-                        $('.contactForm').find("input, textarea").val("");
-                    }
-                    else {
-                        $("#sendmessage").removeClass("show");
-                        $("#errormessage").addClass("show");
-                        $('#errormessage').html(msg);
-                    }
-                    
+                success: function() {
+                  $("#sendmessage").addClass("show");
+                  $("#errormessage").removeClass("show");
+                  $('.contactForm').find("input, textarea").val("");
+                },
+                error: function() {
+                  $("#sendmessage").removeClass("show");
+                  $("#errormessage").addClass("show");
+                  $('#errormessage').html(msg);
                 }
             });
         return false;
--- js/custom.js
+++ js/custom.js
@@ -108,3 +108,18 @@ jQuery(document).ready(function( $ ) {
   });
 
 });
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('#nav-menu-container a').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('#nav-menu-container  ul li a').removeClass("active");
+      currLink.addClass("active");
+    }
+    else{
+      currLink.removeClass("active");
+    }
+  });
+}
